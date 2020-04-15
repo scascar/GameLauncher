14f91f0264120b36b06aa2a18de5414a989b4511
oscar <oscar_blazejewski@yahoo.fr>
2020-04-15 17:53:54 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace GameLauncher
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.currentVersion = new System.Windows.Forms.Label();
+            this.downloadStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonPlay.Location = new System.Drawing.Point(690, 285);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(251, 137);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = false;
+            this.buttonPlay.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 373);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(672, 49);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(414, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Choose folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(672, 226);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // currentVersion
+            // 
+            this.currentVersion.AutoSize = true;
+            this.currentVersion.Location = new System.Drawing.Point(690, 241);
+            this.currentVersion.Name = "currentVersion";
+            this.currentVersion.Size = new System.Drawing.Size(146, 20);
+            this.currentVersion.TabIndex = 8;
+            this.currentVersion.Text = "Current Version: 00";
+            // 
+            // downloadStatus
+            // 
+            this.downloadStatus.AutoSize = true;
+            this.downloadStatus.Location = new System.Drawing.Point(12, 347);
+            this.downloadStatus.Name = "downloadStatus";
+            this.downloadStatus.Size = new System.Drawing.Size(131, 20);
+            this.downloadStatus.TabIndex = 9;
+            this.downloadStatus.Text = "Download Status";
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(953, 434);
+            this.Controls.Add(this.downloadStatus);
+            this.Controls.Add(this.currentVersion);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "MainView";
+            this.Text = "Game Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label currentVersion;
+        private System.Windows.Forms.Label downloadStatus;
+    }
+}
+
